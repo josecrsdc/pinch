55f617c1767ba711b4db8187cfad3600a14d398a
Jose carlos Rodriguez <josecrsdc@gmail.com>
2023-03-04 13:26:30 +0100

    add scale, drag gesture

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jose carlos Rodriguez on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pinch/Pinch/View/InfoPanelView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoPanelView.swift
+//  Pinch
+//
+//  Created by Jose carlos Rodriguez on 4/3/23.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoPanelView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoPanelView()
+    }
+}
